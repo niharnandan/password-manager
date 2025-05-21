1ebda4e0ca7ad18e13631ff164079487aa4706fd
Nihar <nihar.nandan@gmail.com>
2025-05-21 17:45:10 +0530

    Getting deployment to work

--- svelte.config.js
+++ svelte.config.js
@@ -9,7 +9,11 @@ const config = {
       pages: 'build',
       assets: 'build',
       fallback: 'index.html'
-    })
+    }),
+    // Add this line to set the base path for GitHub Pages
+    paths: {
+      base: '/password-manager'
+    }
   }
 };
 
